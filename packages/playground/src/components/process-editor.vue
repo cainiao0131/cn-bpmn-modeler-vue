902b1519d465728837d1bee09d2da05c4ba92ad1
菜鸟 <jinyi@sinosoft.com.cn>
2023-04-03 16:31:29 +0800

    增加版本号；去掉 playground 导入组件语句，插件已安装组件不需要导入语句
    Signed-off-by: 菜鸟 <jinyi@sinosoft.com.cn>

--- packages/playground/src/components/process-editor.vue
+++ packages/playground/src/components/process-editor.vue
@@ -7,7 +7,6 @@
 <script lang="ts" setup>
 import BpmnModeler from 'bpmn-js/lib/Modeler';
 import { InternalEvent, ProcessModelerApi } from '../../../cn-bpmn-modeler/src/types';
-import CnBpmnModeler from '../../../cn-bpmn-modeler/src/cn-bpmn-modeler/cn-bpmn-modeler.vue';
 
 const emit = defineEmits<{
   (eventName: 'api-ready', message: ProcessModelerApi): void;
